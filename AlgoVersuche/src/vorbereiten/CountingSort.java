85cf0d213b32762c51a43ce40ef4bb27b4b9ec5e
Adamddour <106589116+Adamddour@users.noreply.github.com>
2024-01-02 13:41:23 +0100

    Hashing

--- AlgoVersuche/src/vorbereiten/CountingSort.java
+++ AlgoVersuche/src/vorbereiten/CountingSort.java
@@ -1,55 +1,76 @@
 package vorbereiten;
+import java.math.*;
+import java.util.*;
 
 public class CountingSort {
 
-	public static void countingSort(char[] a) {
-		
-		int max=a[0];
-		int min=a[0];
-		
-		//rang berechnen
-		for(int i=0;i<a.length;i++) {
-			
-			if(a[i]>max) {
-				max=a[i];
-			}
-			if(a[i]<min) {
-				min=a[i];
-			}
-			
-		}
-		int rang=max-min+1;
-		int[] count=new int[rang];
-		
-		//Häufigkeit
-		for(int i=0;i<a.length;i++) {
-			count[a[i]-min]++;
-		}
-		
-		//Sortieren
-		
-		int j=0;
-		for(int i=rang-1;i>=0;i--) {
-			
-			while(count[i]>0) {
-				a[j++]=(char)(i+min);
-				count[i]--;
-			}
-			
-		}
-		
-		//Ausgeben
-		for(int i=0;i<a.length;i++) {
-			System.out.print(a[i]);
-			if(i!=a.length-1) {
-				System.out.print(", ");
-			}
-		}
-	}
 	
 	public static void main(String[] args) {
-		char[] a= {'c','b','a','b','f','k','s'};
-		countingSort(a);
+	
+		Scanner sc=new Scanner(System.in);
+		
+		int monat;
+		System.out.println("Bitte Monat eingeben:");
+		monat=sc.nextInt();
+		
+		switch(monat) {
+		case 1:{
+			System.out.println("Januar");
+		}break;
+		
+		case 2:{
+			System.out.println("F");
+		}break;
+		
+		default :{
+			System.out.println("Falsche Eingabe");
+		}
+		
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*double startK=1000;
+		double zinsen=0.023;
+		double zinsBetrag;
+		
+		int jahr=1;
+		
+		while(startK<=2000) {
+			
+			zinsBetrag=startK*zinsen;
+			startK=startK+zinsBetrag;
+			System.out.println("Jahr: "+jahr);
+			jahr++;
+		}
+		*/
+		
+		
+		/*double startK=1000;
+		int jahr=10;
+		double zinsen=0.034;
+		double zinsBetrag=0;
+		
+		for(int i=1;i<=10;i++) {
+			
+			zinsBetrag=startK*zinsen;
+			startK=startK+zinsBetrag;
+			System.out.println("Zins Betrag= "+zinsBetrag+" Endsumme="+startK);
+		}*/
 	}
 	
 }
