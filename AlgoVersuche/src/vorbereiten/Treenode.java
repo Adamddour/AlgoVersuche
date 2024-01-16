a62a44c543522521adf8e36d68bf2811deac7fce
Adam Kaddour <adamkaddour335@gmail.com>
2024-01-16 22:26:14 +0100

    22:26

---
+++ AlgoVersuche/src/vorbereiten/Treenode.java
@@ -0,0 +1,13 @@
+package vorbereiten;
+
+
+public class Treenode {
+
+	Treenode firstChild;
+    Treenode secondChild;
+    int wert;
+
+    public Treenode(int wert) {
+        this.wert = wert;
+    }
+}
